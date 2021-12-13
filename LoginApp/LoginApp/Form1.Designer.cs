47009668748d69018f8ee1c3d70e8e6d86faea75
tuyen5tuoi1412 <75303161+tuyen5tuoi1412@users.noreply.github.com>
2021-12-13 21:28:30 +0700

    first program

---
+++ LoginApp/LoginApp/Form1.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace LoginApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.LoginForm = new System.Windows.Forms.Label();
+            this.EmailBox = new System.Windows.Forms.TextBox();
+            this.PasswordBox = new System.Windows.Forms.TextBox();
+            this.EmailLable = new System.Windows.Forms.Label();
+            this.PasswordLable = new System.Windows.Forms.Label();
+            this.LoginButton = new System.Windows.Forms.Button();
+            this.errorProviderApp = new System.Windows.Forms.ErrorProvider(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.errorProviderApp)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // LoginForm
+            // 
+            this.LoginForm.AutoSize = true;
+            this.LoginForm.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LoginForm.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.LoginForm.Location = new System.Drawing.Point(341, 61);
+            this.LoginForm.Name = "LoginForm";
+            this.LoginForm.Size = new System.Drawing.Size(163, 33);
+            this.LoginForm.TabIndex = 1;
+            this.LoginForm.Text = "Login Form";
+            this.LoginForm.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // EmailBox
+            // 
+            this.EmailBox.BackColor = System.Drawing.SystemColors.Info;
+            this.EmailBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.EmailBox.Location = new System.Drawing.Point(356, 131);
+            this.EmailBox.Name = "EmailBox";
+            this.EmailBox.Size = new System.Drawing.Size(191, 35);
+            this.EmailBox.TabIndex = 2;
+            this.EmailBox.TextChanged += new System.EventHandler(this.EmailBox_TextChanged);
+            this.EmailBox.Validating += new System.ComponentModel.CancelEventHandler(this.EmailBox_Validating);
+            // 
+            // PasswordBox
+            // 
+            this.PasswordBox.BackColor = System.Drawing.SystemColors.Info;
+            this.PasswordBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PasswordBox.Location = new System.Drawing.Point(356, 206);
+            this.PasswordBox.Name = "PasswordBox";
+            this.PasswordBox.Size = new System.Drawing.Size(191, 35);
+            this.PasswordBox.TabIndex = 2;
+            this.PasswordBox.TextChanged += new System.EventHandler(this.PasswordBox_TextChanged);
+            // 
+            // EmailLable
+            // 
+            this.EmailLable.AutoSize = true;
+            this.EmailLable.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.EmailLable.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.EmailLable.ForeColor = System.Drawing.SystemColors.GrayText;
+            this.EmailLable.Location = new System.Drawing.Point(160, 137);
+            this.EmailLable.Name = "EmailLable";
+            this.EmailLable.Size = new System.Drawing.Size(74, 29);
+            this.EmailLable.TabIndex = 1;
+            this.EmailLable.Text = "Email";
+            this.EmailLable.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // PasswordLable
+            // 
+            this.PasswordLable.AutoSize = true;
+            this.PasswordLable.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.PasswordLable.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PasswordLable.ForeColor = System.Drawing.SystemColors.GrayText;
+            this.PasswordLable.Location = new System.Drawing.Point(160, 206);
+            this.PasswordLable.Name = "PasswordLable";
+            this.PasswordLable.Size = new System.Drawing.Size(124, 29);
+            this.PasswordLable.TabIndex = 1;
+            this.PasswordLable.Text = "PassWord";
+            this.PasswordLable.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // LoginButton
+            // 
+            this.LoginButton.BackColor = System.Drawing.Color.Salmon;
+            this.LoginButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LoginButton.Location = new System.Drawing.Point(356, 290);
+            this.LoginButton.Name = "LoginButton";
+            this.LoginButton.Size = new System.Drawing.Size(121, 49);
+            this.LoginButton.TabIndex = 3;
+            this.LoginButton.Text = "Login";
+            this.LoginButton.UseVisualStyleBackColor = false;
+            this.LoginButton.Click += new System.EventHandler(this.LoginButton_Click);
+            // 
+            // errorProviderApp
+            // 
+            this.errorProviderApp.ContainerControl = this;
+            this.errorProviderApp.RightToLeftChanged += new System.EventHandler(this.EmailBox_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(918, 549);
+            this.Controls.Add(this.LoginButton);
+            this.Controls.Add(this.PasswordBox);
+            this.Controls.Add(this.EmailBox);
+            this.Controls.Add(this.PasswordLable);
+            this.Controls.Add(this.EmailLable);
+            this.Controls.Add(this.LoginForm);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.errorProviderApp)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label LoginForm;
+        private System.Windows.Forms.TextBox EmailBox;
+        private System.Windows.Forms.TextBox PasswordBox;
+        private System.Windows.Forms.Label EmailLable;
+        private System.Windows.Forms.Label PasswordLable;
+        private System.Windows.Forms.Button LoginButton;
+        private System.Windows.Forms.ErrorProvider errorProviderApp;
+    }
+}
+
---
+++ LoginApp/LoginApp/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LoginApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void PasswordBox_TextChanged(object sender, EventArgs e)
+        {
+            // set to no text
+           /* PasswordBox.Text = "";*/
+            // the password character is an asterisk
+            PasswordBox.PasswordChar = '*';
+            // the control will allow no more than 14 characters
+            PasswordBox.MaxLength = 14;
+        }
+
+        private void EmailBox_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void EmailBox_Validating(object sender, CancelEventArgs e)
+        {
+            string pattern = "^([0-9a-zA-Z]([-\\.\\w]*[0-9a-zA-Z])*@([0-9a-zA-Z][-\\w]*[0-9a-zA-Z]\\.)+[a-zA-Z]{2,9})$";
+            
+            if (string.IsNullOrWhiteSpace(EmailBox.Text))
+            {
+                e.Cancel = true;
+
+            }
+            else
+            {
+                e.Cancel = false;
+                errorProviderApp.SetError(EmailBox, "");
+            }
+            if (Regex.IsMatch(EmailBox.Text, pattern))
+            {
+                errorProviderApp.Clear();
+            }
+            else
+            {
+                EmailBox.Focus();
+                MessageBox.Show("Please enter the correct email form", "Alert Login");
+                errorProviderApp.SetError(this.EmailBox, "Email is not correct fomat");
+            }
+        }
+
+
+        private void LoginButton_Click(object sender, EventArgs e)
+        {
+           
+            if (ValidateChildren(ValidationConstraints.Enabled))
+            {
+                MessageBox.Show($"Email:{EmailBox} and {PasswordBox}", "Message Login!");
+            }
+           
+        }
+    }
+}
